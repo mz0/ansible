33c55e871c7655028ee6fe39c969346927e0a1d8
mz0 <marcuzero@gmail.com>
2018-12-06 16:41:35 +0300

    try (broken!) module w/hardcoded command

--- lib/ansible/modules/cloud/aruba_smart/aruba_smart_vm.py
+++ lib/ansible/modules/cloud/aruba_smart/aruba_smart_vm.py
@@ -1,71 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Copyright: (c) 2018, Mark Zhitomirsky <mz@exactpro.com>
+# Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-ANSIBLE_METADATA = {
-    'metadata_version': '1.1',
-    'status': ['preview'],
-    'supported_by': 'community'
-}
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
 
 DOCUMENTATION = '''
----
-module: aruba_smart_vm
-
-short_description: This is my sample module
-
-version_added: "2.8"
-
-description:
-    - "This is my longer description explaining my sample module"
-
-options:
-    user:
-        description:
-            - Aruba Cloud ClientID
-        required: true
-    password:
-        description:
-            - Aruba Cloud management password
-        required: true
-    dc:
-        description:
-            - List of DCs or DC name
-        required: false
-    size:
-        description:
-            - choose one of S,M,L,XL (vCPU-RAM-SSD-Traffic, Eur per month)
-            - S - 1-1GB-020GB-02TB, 2.79
-            - M - 1-2GB-040GB-05TB, 4.50
-            - L - 2-4GB-080GB-12TB, 8.50
-            - XL- 4-8GB-160GB-25TB, 15.0
-    root_pw:
-        description:
-            - root password, will be auto-generated?
-        required: false
-
-extends_documentation_fragment:
-    - aruba_cloud
-
-author:
-    - Mark Zhitomirski (@mz0)
 '''
 
 EXAMPLES = '''
-# list VMs
-- name: Test with a message and changed output
-  aruba_smart_vm:
-    name: hello world
-    new: true
 '''
 
 RETURN = '''
-data:
-    description: List of VMs
-    type: dict
-sample: { FIXME
-}
 '''
 import json
 
@@ -99,9 +52,8 @@ class Response(object):
 
 class ArubaCloudAPI(object):
 
-    def __init__(self, module, headers):
+    def __init__(self, module):
         self.module = module
-        self.headers = headers
         self.auser = module.params.get('user')
         self.passwd = module.params.get('password')
         self.timeout = module.params.get('timeout', 30)
@@ -110,54 +62,43 @@ class ArubaCloudAPI(object):
     def _url_builder(self, dc, cmd):
         return self.tpl_url.format(dc, cmd)
 
-    def send(self, method, dc, cmd, data=None):
+    def send(self, method, dc, cmd, data):
         url = self._url_builder(dc, cmd)
         data = self.module.jsonify(data)
         timeout = self.module.params['timeout']
         headers = {'Content-Type': 'application/json', 'Content-Length': str(len(data))}
         resp, info = fetch_url(self.module, url, data=data, headers=headers, method=method, timeout=timeout)
 
-        # Exceptions in fetch_url may result in a status -1, the ensures a
-        if info['status'] == -1:
-            self.module.fail_json(msg=info['msg'])
-
         return Response(resp, info)
 
-    def post(self, dc, cmd, data=None):
-        return self.send('POST', dc, cmd, data, headers)
+    def post(self, dc, cmd, data):
+        return self.send('POST', dc, cmd, data)
 
 
 def core(module):
     auser = module.params['user']
     apassw = module.params['password']
-#   state = module.params['state']
+    state = module.params['state']
 #   name = module.params['name']
 #   ssh_pub_key = module.params['ssh_pub_key']
     cmd = "GetServers"
     cmd_data = '{{"ApplicationId": "{}", "RequestId": "{}", "SessionId": "{}", "Password": "{}", "Username": "{}"}}'\
-        .format(cmd,cmd,cmd,apassw,auser)
-    reqest = ArubaCloudAPI(module, "dc1", cmd, cmd_data)
-
-    module.exit_json(**result)
+        .format(cmd, cmd, cmd, apassw, auser)
+    api = ArubaCloudAPI(module)
+    response = api.post(dc="dc1", cmd=cmd, data=cmd_data)
+    json1 = response.json
+    module.exit_json(changed=False, data=json1)
 
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            state=dict(choices=['present', 'absent'], default='present'),
-            name=dict(required=False),
-            ssh_pub_key=dict(required=False),
-            creds=dict(
-                no_log=True,
-                required=True,
-            ),
-            validate_certs=dict(type='bool', default=True),
+            state=dict(choices=['present', 'absent', 'fresh'], default='present'),
+            user=dict(required=True),
+            password=dict(no_log=True, required=True),
             timeout=dict(type='int', default=30),
         ),
-        required_one_of=(
-            ('fingerprint', 'ssh_pub_key'),
-        ),
-        supports_check_mode=True,
+        supports_check_mode=False,
     )
 
     core(module)
