ac8f9eca69cf5092401fbad557d375883c5c3071
mz0 <marcuzero@gmail.com>
2019-02-07 21:36:22 +0300

    re-wire rebuild option as rebuild: yes; refactor
    - warn when parameters make no sense (both id and name supplied)
    - rename get_addresses() to expose_addresses()
    - kill trivial core() and move its contents to main().
    - add size parameter default, since that's the most obvious thing
      one wants, if desired size can be upgraded later.
    - kill more temp vars, esp. those with elusive semantics.
    - edit docs

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet.py
@@ -25,7 +25,7 @@ options:
     description:
      - Indicate desired state of the target.
     default: present
-    choices: ['present', 'pristine', 'absent']
+    choices: ['present', 'absent']
   id:
     description:
      - Numeric, the droplet id you want to operate on.
@@ -35,25 +35,24 @@ options:
      - String, this is the name of the droplet - must be a valid hostname or a FQDN.
   unique_name:
     description:
-     - require unique hostnames.  By default, DigitalOcean allows multiple hosts with the same name.  Setting this to "yes" allows only one host
-       per name.  Useful for idempotence.
-    default: False
-    type: bool
+     - deprecated and ignored parameter, consider it always True.
+    required: False
   size:
     description:
-     - This is the slug of the size you would like the droplet created with.
+     - The slug of the desired droplet configuration, e.g. 's-1vcpu-1gb', '2gb', 'c-32vcpu-64gb', or 's-32vcpu-192gb'.
     aliases: ['size_id']
+    default: 's-1vcpu-1gb'
   image:
     description:
      - This is the slug of the image you would like the droplet created with.
     aliases: ['image_id']
   region:
     description:
-     - This is the slug of the region you would like your server to be created in.
+     - The slug of the datacenter you would like your droplet to be created in, e.g. 'sfo2', 'ams3', or 'sgp1'.
     aliases: ['region_id']
   ssh_keys:
     description:
-     - array of SSH key (numeric) ID that you would like to be added to the server.
+     - list of SSH key numeric IDs or fingerprints to put in ~root/authorized_keys on creation.
     required: False
   private_networking:
     description:
@@ -62,17 +61,17 @@ options:
     type: bool
   user_data:
     description:
-      - opaque blob of data which is made available to the droplet
+      - string data >64KB, e.g. a 'cloud-config' file or a Bash script to configure the Droplet on first boot.
     required: False
   ipv6:
     description:
-      - enable IPv6 for your droplet.
+      - enable IPv6 for new droplet.
     required: False
     default: False
     type: bool
   wait:
     description:
-     - Wait for the droplet to be active before returning.  If wait is "no" an ip_address may not be returned.
+     - Wait for the droplet to be active before returning.
     required: False
     default: True
     type: bool
@@ -94,7 +93,7 @@ options:
     type: bool
   tags:
     description:
-     - List, A list of tag names as strings to apply to the Droplet after it is created. Tag names can either be existing or new tags.
+     - List, A list of tag names as strings to apply to the Droplet after it is created. A tag can be existing or new.
     required: False
   volumes:
     description:
@@ -111,37 +110,35 @@ requirements:
 
 
 EXAMPLES = '''
-- name: create a new droplet
+- name: create new or find existing droplet
   digital_ocean_droplet:
     state: present
-    name: mydroplet
-    oauth_token: XXX
-    size: 2gb
+    name: mydroplet.example.com
+    oauth_token: "{{ lookup('file', '~/.do/api-key1') }}"
+    size: 1gb
     region: sfo1
     image: ubuntu-16-04-x64
+    tags: [ 'foo', 'bar' ]
     wait_timeout: 500
   register: my_droplet
 
 - debug:
     msg: "ID is {{ my_droplet.data.droplet.id }}, IP is {{ my_droplet.data.ip_address }}"
 
-- name: ensure a droplet is present
+- name: Check droplet exists, get details
   digital_ocean_droplet:
-    state: present
-    id: 123
-    name: mydroplet
-    oauth_token: XXX
-    size: 2gb
-    region: sfo1
-    image: ubuntu-16-04-x64
-    wait_timeout: 500
+    name: mydroplet.example.com
+    oauth_token: "{{ lookup('file', '~/.do/api-key1') }}"
+  register: my_droplet
 
 - name: ensure a droplet is like new
   digital_ocean_droplet:
-    state: pristine
     name: mydroplet.example.com
-    oauth_token: XXX
-    image: debian-9-x64  # optional, may be omitted.
+    oauth_token: "{{ lookup('file', '~/.do/api-key1') }}"
+    rebuild: yes
+    image: debian-9-x64  # may be omitted.
+    wait_timeout: 240
+  register: my_droplet
 '''
 
 
@@ -157,58 +154,61 @@ data:
         "private_ipv4_address": "10.136.122.141",
         "droplet": {
             "id": 3164494,
-            "name": "example.com",
-            "memory": 512,
+            "name": "mydroplet.example.com",
+            "memory": 1024,
             "vcpus": 1,
-            "disk": 20,
-            "locked": true,
-            "status": "new",
-            "kernel": {
-                "id": 2233,
-                "name": "Ubuntu 14.04 x64 vmlinuz-3.13.0-37-generic",
-                "version": "3.13.0-37-generic"
-            },
+            "disk": 25,
+            "locked": false,
+            "status": "active",
+            "kernel": null,
             "created_at": "2014-11-14T16:36:31Z",
-            "features": ["virtio"],
+            "features": ["private_networking", "ipv6"],
             "backup_ids": [],
             "snapshot_ids": [],
-            "image": {},
+            "image": {"slug": "debian-9-x64", ...},
             "volume_ids": [],
-            "size": {},
-            "size_slug": "512mb",
-            "networks": {},
-            "region": {},
-            "tags": ["web"]
+            "size": {"transfer": 1.0, "price_monthly": 5.0, "price_hourly": 0.00744, ...},
+            "size_slug": "1gb",
+            "networks": {"v4": [{"type": "public", ...}, {"type": "private", ...},], "v6": [{"type": "public", ...}]},
+            "region": {"slug":"sfo1",...},
+            "tags": []
         }
     }
 '''
 
 import time
-import json
 from ansible.module_utils.basic import AnsibleModule, env_fallback
 from ansible.module_utils.digital_ocean import DigitalOceanHelper
 
 
 class DODroplet(object):
     def __init__(self, module):
-        self.rest = DigitalOceanHelper(module)
         self.module = module
+        self._id = self.module.params['id']
+        self._name = self.module.params['name']
+        self._droplet = None  # == _id if found by _id
+        self.rest = DigitalOceanHelper(module)
+        # pop all the parameters which we never POST as data
+        self.rebuild = self.module.params.pop('rebuild')
         self.wait = self.module.params.pop('wait', True)
         self.wait_timeout = self.module.params.pop('wait_timeout', 120)
-        self.unique_name = self.module.params.pop('unique_name', False)
-        # pop the oauth token so we don't include it in the POST data
         self.module.params.pop('oauth_token')
+        if self.module.params.pop('unique_name', None) is not None:
+            self.module.warn("Parameter `unique_name` is deprecated. Consider it's always True.")
+        if self._id and self._name:
+            self.module.warn("Both id {} and name {} supplied. Your play may turn unexpectedly!".
+                             format(self._id, self._name))
 
     def get_by_id(self, droplet_id):
         if not droplet_id:
             return None
         response = self.rest.get('droplets/{0}'.format(droplet_id))
-        json_data = response.json
         if response.status_code == 200:
-            return json_data
+            self._droplet = droplet_id
+            return response.json
         return None
 
-    def get_by_name(self, droplet_name):
+    def find_by_name(self, droplet_name):
         if not droplet_name:
             return None
         page = 1
@@ -218,6 +218,7 @@ class DODroplet(object):
             if response.status_code == 200:
                 for droplet in json_data['droplets']:
                     if droplet['name'] == droplet_name:
+                        self._droplet = droplet_name
                         return {'droplet': droplet}
                 if 'links' in json_data and 'pages' in json_data['links'] and 'next' in json_data['links']['pages']:
                     page += 1
@@ -225,7 +226,7 @@ class DODroplet(object):
                     page = None
         return None
 
-    def get_addresses(self, data):
+    def expose_addresses(self, data):
         """
          Expose IP addresses as their own property allowing users extend to additional tasks
         """
@@ -245,33 +246,56 @@ class DODroplet(object):
                 _data['private_ipv6_address'] = network['ip_address']
         return _data
 
-    def get_droplet(self):
-        json_data = self.get_by_id(self.module.params['id'])
-        if not json_data and self.unique_name:
-            json_data = self.get_by_name(self.module.params['name'])
+    def find_droplet(self):
+        json_data = self.get_by_id(self._id)
+        if not json_data:
+            json_data = self.find_by_name(self._name)
         return json_data
 
-    def create(self):
-        json_data = self.get_droplet()
-        droplet_data = None
-        if json_data:
-            droplet_data = self.get_addresses(json_data)
-            self.module.exit_json(changed=False, data=droplet_data)
+    def _params_ok(self, name, image, region):
+        """
+        When creating droplet we need at least name, image, and region.
+        :return:
+        True if all the parameters were supplied.
+        """
+        return name and image and region
+
+    def get(self):
+        """
+        Find the droplet (either by id or name), rebuild if requested so, build if not found.
+        """
+        json_data = self.find_droplet()
+        _size = self.module.params['size']
+        _image = self.module.params['image']
+        _region = self.module.params['region']
+        if json_data and not self.rebuild:
+            self.module.exit_json(changed=False, data=self.expose_addresses(json_data))
+        elif self.rebuild and not json_data:
+            self.module.fail_json(changed=False, msg='droplet {} not found. Rebuild failed.'.format(self._droplet))
+        elif self.rebuild and json_data:
+            self._rebuild(json_data)  # _rebuild() should not return here.
+        # we are going to build a new droplet now. Final checks.
+        if self._id:
+            self.module.warn("Trying to build {}. Parameter id is found, it makes no sense here!".format(self._name))
+        if not _size:
+            _default_size = 's-1vcpu-1gb'
+            self.module.warn("Missing 'size' parameter. Using size={}".format(_default_size))
+            _size = _default_size
+        if not self._params_ok(self._name, _image, _region):
+            self.module.fail_json(changed=False, msg='Droplet not created. Not enough parameters: name={} region={}'
+                                                     ' image={} size={}'.format(self._name, _region, _image, _size))
         if self.module.check_mode:
             self.module.exit_json(changed=True)
         response = self.rest.post('droplets', data=self.module.params)
-        json_data = response.json
         if response.status_code >= 400:
-            self.module.fail_json(changed=False, msg=json_data['message'])
+            self.module.fail_json(changed=False, msg=response.json['message'])
         if self.wait:
-            json_data = self.ensure_active(json_data['droplet']['id'])
-            droplet_data = self.get_addresses(json_data)
-        self.module.exit_json(changed=True, data=droplet_data)
+            self.ensure_active(response.json['droplet']['id'])
+        self.module.exit_json(changed=True, data=self.expose_addresses(response.json))
 
-    def rebuild(self):
-        json_data = self.get_droplet()
-        if not json_data:
-            self.module.fail_json(changed=False, msg='Rebuild: droplet not found')
+    def _rebuild(self, json_data):
+        if self._id and self._name:
+            self.module.warn("Trying to rebuild droplet id={}. Parameter name is found too, it makes no sense here!".format(self._id))
         if self.module.check_mode:
             self.module.exit_json(changed=True)
         droplet_id = json_data['droplet']['id']
@@ -284,16 +308,15 @@ class DODroplet(object):
         if response.status_code >= 400:
             self.module.fail_json(changed=False, msg=json_data['message'])
         json_data = self.ensure_unlocked(droplet_id)  # wait for rebuild to finish
-        droplet_data = self.get_addresses(json_data)
+        droplet_data = self.expose_addresses(json_data)
         self.module.exit_json(changed=True, data=droplet_data)
 
     def delete(self):
-        json_data = self.get_droplet()
+        json_data = self.find_droplet()
         if json_data:
             if self.module.check_mode:
                 self.module.exit_json(changed=True)
             response = self.rest.delete('droplets/{0}'.format(json_data['droplet']['id']))
-            json_data = response.json
             if response.status_code == 204:
                 self.module.exit_json(changed=True, msg='Droplet deleted')
             self.module.fail_json(changed=False, msg='Failed to delete droplet')
@@ -318,24 +341,13 @@ class DODroplet(object):
             if json_data['droplet']['status'] == 'active':
                 return json_data
             time.sleep(min(2, end_time - time.time()))
-        self.module.fail_json(msg='Wait for droplet powering on timeout')
-
-
-def core(module):
-    state = module.params.pop('state')
-    droplet = DODroplet(module)
-    if state == 'present':
-        droplet.create()
-    elif state == 'pristine':
-        droplet.rebuild()
-    elif state == 'absent':
-        droplet.delete()
+        self.module.fail_json(msg='Wait for droplet status=active timeout')
 
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            state=dict(choices=['present', 'pristine', 'absent'], default='present'),
+            state=dict(choices=['present', 'absent'], default='present'),
             oauth_token=dict(
                 aliases=['API_TOKEN'],
                 no_log=True,
@@ -344,6 +356,7 @@ def main():
             name=dict(type='str'),
             size=dict(aliases=['size_id']),
             image=dict(aliases=['image_id']),
+            rebuild=dict(type='bool', default=False),
             region=dict(aliases=['region_id']),
             ssh_keys=dict(type='list'),
             private_networking=dict(type='bool', default=False),
@@ -356,18 +369,19 @@ def main():
             tags=dict(type='list'),
             wait=dict(type='bool', default=True),
             wait_timeout=dict(default=120, type='int'),
-            unique_name=dict(type='bool', default=False),
+            unique_name=dict(),
         ),
         required_one_of=(
             ['id', 'name'],
         ),
-        required_if=([
-            ('state', 'present', ['name', 'size', 'image', 'region']),
-        ]),
         supports_check_mode=True,
     )
-
-    core(module)
+    state = module.params.pop('state')
+    droplet = DODroplet(module)
+    if state == 'present':
+        droplet.get()
+    elif state == 'absent':
+        droplet.delete()
 
 
 if __name__ == '__main__':
